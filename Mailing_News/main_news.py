9e92f5d476a014567e8863180f26c0575fecd9ec
MoustachedCoder <yuvrajwwe1234@gmail.com>
2026-03-30 20:07:07 +0530

    Add files via upload

---
+++ Mailing_News/main_news.py
@@ -0,0 +1,127 @@
+# CREATED BY ANDREW GEORGE ISSAC
+
+#HOW TO USE?
+# WHEN GMAIL IS USED FOR SENDING DATA, PLEASE MAKE SURE THAT YOU HAVE TURNED ON LESS SECURE APP ACCESS IN THE SECURITY SETTINGS OF YOUR GMAIL ACCOUNT MANAGER!
+
+# RUN THE NEWS FUNCTION WHICH YOU LIKE & THEN RUN THE MAILING FUNCTION!
+
+#NOTE: IT TAKES UPTO 40-50sec TO COMPLETE THE PROCESS OF SCRACPING & SENDING.
+#NOTE: THE RECIEPIENT MAIL ADDRESS CAN BE ANY MAIL FROM ANY PROVIDER!
+#COMMENT YOUR SUGGESTIONS, DOUBTS, PROBLEMS @ GitHub
+
+
+
+
+from selenium import webdriver
+import smtplib
+
+
+PATH="Mailing_news/geckodriver.exe"
+
+options=webdriver.FirefoxOptions()
+options.add_argument('-headless')
+
+driver=webdriver.Firefox(executable_path=PATH,options=options)
+
+headlines=''
+top_stories=[]
+news_name=''
+
+
+def TOI(): #Times_of_India
+    global top_stories
+    global headlines
+    global news_name
+    top_stories=[]
+    news_name='TimesOfIndia'
+    driver.get('https://timesofindia.indiatimes.com')
+    print('driver_optimized')
+    headlines=driver.find_element_by_xpath('//*[@id="featuredstory"]/h2/a').text
+    
+    i=1
+    try:
+        while True:
+            stry=driver.find_element_by_xpath('//*[@id="content"]/div/div[6]/ul/li['+str(i)+']/a[1]').text
+            top_stories.append('\n'+str(i)+'. '+stry)
+            i=i+1    
+    except:
+        pass
+    print('--scrap_complete')
+
+    
+
+def india_today(): #India_Today
+    global top_stories
+    global headlines
+    global news_name
+    news_name='IndiaToday'
+    top_stories=[]
+    driver.get('https://www.indiatoday.in/')
+    print('driver_optimized')
+    headlines=driver.find_element_by_xpath('//*[@id="block-itg-widget-home-page-feature"]/div/div[1]/h2/a').text
+    
+    try:
+        for i in range(1,6):
+            stry=driver.find_element_by_xpath('//*[@id="block-itg-widget-top-stories-ordering"]/ul/li['+str(i)+']/a').text
+            top_stories.append(str(i)+'. '+stry+'\n')
+    except:
+        pass
+    print('--scrap_complete')
+   
+
+def indian_express(): #Indian_Express
+    global top_stories
+    global headlines
+    global news_name
+    news_name='IndianExpress'
+    top_stories=[]
+    driver.get('https://indianexpress.com')
+    print('driver_optimized')
+    headlines=driver.find_element_by_xpath('//*[@id="HP_TOP_NEWS_STORIES"]/div[1]/div[1]/div[1]/h2/a').text
+    try:
+        for i in range(1,6):
+            stry=driver.find_element_by_xpath('//*[@id="HP_TOP_NEWS_STORIES"]/div[2]/div/ul/li['+str(i)+']/h3/a').text
+            top_stories.append(str(i)+'. '+stry+'\n')
+    except:
+        pass
+    print('--scrap_complete')
+
+
+
+def gmail_send():  #gmail_function
+    SUBJECT='The News Boy! '+news_name
+    BODY='Breaking News:'+headlines+'\n'+'\n--Top Stories--\n'
+    for n in range(len(top_stories)):
+        BODY=BODY+top_stories[n]
+    BODY=BODY+'\n'+'\n'+'With Love,'+'\n'+'NewsBoy_Initiative(By Andrew)'
+    message = 'Subject: {}\n\n{}'.format(SUBJECT, BODY).encode('utf-8')
+    mail=smtplib.SMTP('smtp.gmail.com',587)
+    mail.ehlo()
+    mail.starttls()
+    mail.ehlo()
+    mail.login('your_gmail','your_gmail_password')
+    mail.sendmail('your_gmail','receipient_mail_address',message)
+    mail.close()
+    print('mailing complete!')
+
+
+def outlook_send():  #outlook_function
+    SUBJECT='The News Boy! '+news_name
+    BODY='Breaking News:'+headlines+'\n'+'\n--Top Stories--\n'
+    for n in range(len(top_stories)):
+        BODY=BODY+top_stories[n]
+    BODY=BODY+'\n'+'\n'+'With Love,'+'\n'+'NewsBoy_Initiative(By Andrew)'
+    message = 'Subject: {}\n\n{}'.format(SUBJECT, BODY).encode('utf-8')
+    mail=smtplib.SMTP('smtp-mail.outlook.com',587)
+    mail.ehlo()
+    mail.starttls()
+    mail.ehlo()
+    mail.login('your_outlook_mail','your_outlook_mail_password')
+    mail.sendmail('your_outlook_mail','recipient_email_address',message)
+    mail.close()
+    print('mailing complete!')
+
+
+
+TOI()
+gmail_send()
--- README.md
+++ README.md
@@ -1,35 +1,60 @@
-# News-scrapper-for-email-newsletter
-The News-scrapper is a python program to send Email of latest news & top-stories scrapped from various sites, It uses geckodriver, BeautifulSoup to access the websites.
-
-Build Status Known Vulnerabilities codecov.io Code Coverage
-
-Outlook mail provision added!
-
-Libraries Used
-Selenium
-smtplib
-BeautifulSoup
-gtts
-Driver & Browser
-Geckodriver (driver)
-Mozilla Firefox (browser--latest version)
-Installation
+# The News Boy 
+ The News Boy is a python program to send Email of latest news & top-stories scrapped from various sites, create podcasts from scrapped data, and many more! It uses geckodriver, BeautifulSoup to access the websites.
+ 
+ [![Build Status](https://travis-ci.org/dwyl/esta.svg?branch=master)](https://travis-ci.org/dwyl/esta) ![Known Vulnerabilities](https://snyk.io/test/github/dwyl/hapi-auth-jwt2/badge.svg?targetFile=package.json) ![codecov.io Code Coverage](https://img.shields.io/codecov/c/github/dwyl/hapi-auth-jwt2.svg?maxAge=2592000)
+ # Releases 
+ Latest Release: v1.1.0
+ 
+ All releases can be found at the releases page!
+ 
+ # Updates
+ >New feature: Business news
+ 
+ > Outlook mail provision added!
+ 
+ # Libraries Used
+ 1. Selenium
+ 2. smtplib 
+ 3. BeautifulSoup
+ 4. gtts
+ 
+ # Driver & Browser
+ 1. Geckodriver (driver)
+ 2. Mozilla Firefox (browser--latest version)
+ 
+ # Installation
+```
 pip install selenium
+```
+```
 pip install secure-smtplib
+```
+```
 pip install gTTS
+```
+```
 pip install beautifulsoup4
-Data
-Times of India:https://timesofindia.indiatimes.com/
+```
 
-Times of India(Business): https://timesofindia.indiatimes.com/business/india-business
+# Data
+Times of India:<https://timesofindia.indiatimes.com/>
 
-IndiaToday: https://www.indiatoday.in/
+Times of India(Business): <https://timesofindia.indiatimes.com/business/india-business>
 
-IndianExpress: https://indianexpress.com
+IndiaToday: <https://www.indiatoday.in/>
 
-BloombergQuint: https://www.bloombergquint.com
+IndianExpress: <https://indianexpress.com>
 
-Mailing
-Sometimes the mail received may be on the junk folder. Please have a look on it.
+BloombergQuint: <https://www.bloombergquint.com>
 
-As of now mails can be sent only from Gmail.
+# Mailing 
+>Sometimes the mail received may be on the junk folder. Please have a look on it.
+
+>As of now mails can be sent only from Gmail.
+
+# About & Contact
+Created by: Andrew George Issac
+
+Instagram: <https://www.instagram.com/_andrewissac>
+
+Twitter: <https://twitter.com/andrewgeorge002>
